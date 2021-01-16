5441f5f7878741635806efaa0bee632a8d0d440a
aubrey-heim <aubrey.l.heim@gmail.com>
2021-01-15 20:04:34 -0800

    set up html routes

---
+++ routes/html.js
@@ -0,0 +1,15 @@
+const path = require("path")
+
+module.exports = function(app) {
+    app.get("/", function(req, res){
+        res.sendFile(path.join(__dirname, "/index.html"));
+    })
+
+    app.get("/exercise", function(req, res){
+        res.sendFile(path.join(__dirname, "/exercise.html"));
+    })
+
+    app.get("/stats", function(req, res){
+        res.sendFile(path.join(__dirname, "/stats.html"));
+    })
+}
