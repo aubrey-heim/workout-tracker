f11e2b45226fe9144c1e2ec4797c53acac2fc6aa
aubrey-heim <aubrey.l.heim@gmail.com>
2021-01-17 17:33:31 -0800

    set up API routes

---
+++ routes/api.js
@@ -0,0 +1,60 @@
+const db = require("../models");
+var router = require('express').Router();
+
+router.put("/api/workouts/:id", function(req,res){
+    const idNeeded = req.params.id
+    db.Workout.findOneAndUpdate({ _id: idNeeded }, { $push: { exercises: req.body } }, { new: true })
+    .then(Workout => {
+        res.json(Workout);
+    })
+    .catch(err => {
+        res.status(400).json(err);
+    });
+})
+
+router.post("/api/workouts", function(req,res){
+    db.Workout.create(req.body)
+    .then(results => {
+        res.json(results);
+    })
+    .catch(err => {
+        res.status(400).json(err);
+    });
+})
+
+router.get("/api/workouts", function(req, res){
+
+    db.Workout.aggregate([
+        {
+          $addFields: {
+            totalDuration: { $sum: "$exercises.duration" } ,
+          }
+        }
+    ])
+    .then(Workout => {
+        res.json(Workout);
+    })
+    .catch(err => {
+        res.status(400).json(err);
+    });
+})  
+
+router.get("/api/workouts/range", function(req, res){
+    db.Workout.aggregate([
+        {
+          $addFields: {
+            totalDuration: { $sum: "$exercises.duration" } ,
+          }
+        }
+    ])
+    .limit(7)
+    .sort({day: "desc"})
+    .then(Workout => {
+        res.json(Workout);
+    })
+    .catch(err => {
+        res.status(400).json(err);
+    });
+})
+
+module.exports = router
